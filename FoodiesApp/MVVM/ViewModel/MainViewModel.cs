ba9c8ffd6c7db73853179a280d9238aaea81fb5e
Kingnova5546 <marquis5546@yahoo.com>
2021-10-06 23:56:47 -0700

    Alot of graphical updates. Just starting grounds nothing solid!

---
+++ FoodiesApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using FoodiesApp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodiesApp.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
